bc9ce40dcbba582eecb99f496e80a15614f71aec
Chen HaiYong <Chyrain@users.noreply.github.com>
2017-04-04 22:13:15 +0800

    Create index.js
    Add default

---
+++ index.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  name: {
+    first: 'Chen',
+    middle: 'Hai',
+    last: 'Yong',
+  },
+  birthday: '1991-08-10',
+  twitter: 'https://twitter.com/chyrain',
+  github: 'https://github.com/chyrain',
+  website: 'http://chyrain.com',
+  email: 'chyrain_v5kf@qq.com'
+}
